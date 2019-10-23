f95cf6e26a2533e04abe997a4b10d6b4a6f39f85
Emre Sen <emre.sen@cantekgroup.com>
2019-10-23 15:11:56 +0300

    fix: ACrudServiceImpl improvments
    - in create method of ACrudServiceImpl, is entity exists check added.

--- src/main/java/cloud/cantek/ms/core/service/ACrudServiceImpl.java
+++ src/main/java/cloud/cantek/ms/core/service/ACrudServiceImpl.java
@@ -13,152 +13,159 @@ import cloud.cantek.ms.core.exception.ServiceException;
 
 /**
  * Abstract Implementation of ICrudService.
- * 
+ * <p>
  * Basic CRUD operations for an entity is implemented.
- * 
+ *
  * @param <E>  type entity
  * @param <ID> type of identity of entity
- *
  * @author Emre Sen, 19.07.2019
  * @contact maemresen07@gmail.com
- *
  */
 public abstract class ACrudServiceImpl<E, ID> implements ICrudService<E, ID> {
 
-	private final Logger LOGGER = LoggerFactory.getLogger(ACrudServiceImpl.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(ACrudServiceImpl.class);
 
-	/**
-	 * Repository of the entity that will be used with this implementation
-	 * 
-	 */
-	public abstract JpaRepository<E, ID> getRepository();
+    /**
+     * Repository of the entity that will be used with this implementation
+     */
+    public abstract JpaRepository<E, ID> getRepository();
 
-	public E save(E entity) throws ServiceException {
+    public E save(E entity) throws ServiceException {
 
-		// get repo
-		JpaRepository<E, ID> repository = getRepository();
+        // get repo
+        JpaRepository<E, ID> repository = getRepository();
 
-		try {
-			// save entity
-			return repository.save(entity);
-		} catch (Exception exception) {
-			throw new ServiceException(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_SAVE, exception);
-		}
-	}
+        try {
+            // save entity
+            return repository.save(entity);
+        } catch (Exception exception) {
+            throw new ServiceException(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_SAVE, exception);
+        }
+    }
 
-	@Override
-	public E create(E entity) throws ServiceException {
+    @Override
+    public E create(E entity) throws ServiceException {
 
-		// initialize entity to insert
-		// e.g setting unique UUID
-		E initializedEntity = preInsert(entity);
+        // get repo
+        JpaRepository<E, ID> repository = getRepository();
 
-		return save(initializedEntity);
-	}
+        ID id = getId(entity);
+        boolean eventExists = repository.existsById(id);
+        if (eventExists) {
+            throw new ServiceException(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_SAVE);
+        }
 
-	/**
-	 * Operations before insert entity to the table
-	 * 
-	 * E.g.: setting uuid value
-	 * 
-	 * @param entity values that will be inserted
-	 * @return entity that inserted to the data source
-	 */
-	protected abstract E preInsert(E entity);
 
-	@Override
-	public List<E> getAll() throws ServiceException {
+        // initialize entity to insert
+        // e.g setting unique UUID
+        E initializedEntity = preInsert(entity);
 
-		// get repo
-		JpaRepository<E, ID> repository = getRepository();
-		try {
-			return repository.findAll();
-		} catch (Exception exception) {
-			throw new ServiceException(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_GET_LIST, exception);
-		}
-	}
+        return save(initializedEntity);
+    }
 
-	@Override
-	public E getById(ID id) throws ServiceException, NotFoundException {
+    /**
+     * Operations before insert entity to the table
+     * <p>
+     * E.g.: setting uuid value
+     *
+     * @param entity values that will be inserted
+     * @return entity that inserted to the data source
+     */
+    protected abstract E preInsert(E entity);
 
-		// get repo
-		JpaRepository<E, ID> repository = getRepository();
-		Optional<E> entityOptional;
+    @Override
+    public List<E> getAll() throws ServiceException {
 
-		try {
-			// find entity by id
-			entityOptional = repository.findById(id);
-		} catch (Exception exception) {
-			throw new ServiceException(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_GET_BY_ID, exception);
-		}
+        // get repo
+        JpaRepository<E, ID> repository = getRepository();
+        try {
+            return repository.findAll();
+        } catch (Exception exception) {
+            throw new ServiceException(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_GET_LIST, exception);
+        }
+    }
 
-		// get entity or throw exception if empty
-		if (entityOptional.isPresent()) {
+    @Override
+    public E getById(ID id) throws ServiceException, NotFoundException {
 
-			return entityOptional.get();
-		}
+        // get repo
+        JpaRepository<E, ID> repository = getRepository();
+        Optional<E> entityOptional;
 
-		LOGGER.error(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_NOT_FOUND);
-		throw new NotFoundException(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_NOT_FOUND);
-	}
+        try {
+            // find entity by id
+            entityOptional = repository.findById(id);
+        } catch (Exception exception) {
+            throw new ServiceException(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_GET_BY_ID, exception);
+        }
 
-	@Override
-	public E update(E newEntity) throws ServiceException {
+        // get entity or throw exception if empty
+        if (entityOptional.isPresent()) {
 
-		// get old entity
-		ID id = getId(newEntity);
-		E oldEntity = getById(id);
+            return entityOptional.get();
+        }
 
-		// prepare entity for update
-		E preparedEntity = preUpdate(oldEntity, newEntity);
+        LOGGER.error(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_NOT_FOUND);
+        throw new NotFoundException(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_NOT_FOUND);
+    }
 
-		return save(preparedEntity);
-	}
+    @Override
+    public E update(E newEntity) throws ServiceException {
 
-	/**
-	 * Get Id of the entity
-	 * 
-	 * @param entity input entity model
-	 * @return id of entity
-	 */
-	protected abstract ID getId(E entity);
+        // get old entity
+        ID id = getId(newEntity);
+        E oldEntity = getById(id);
 
-	/**
-	 * Operations before update operation
-	 * 
-	 * @param oldEntity old data on data source
-	 * @param newEntity new data that will be saved to data source
-	 * @return
-	 */
-	protected abstract E preUpdate(E oldEntity, E newEntity);
+        // prepare entity for update
+        E preparedEntity = preUpdate(oldEntity, newEntity);
 
-	@Override
-	public boolean delete(E entity) {
-		try {
+        return save(preparedEntity);
+    }
 
-			// get repo
-			JpaRepository<E, ID> repository = getRepository();
+    /**
+     * Get Id of the entity
+     *
+     * @param entity input entity model
+     * @return id of entity
+     */
+    protected abstract ID getId(E entity);
 
-			// delete entity
-			repository.delete(entity);
-			return true;
-		} catch (Exception exception) {
-			throw new ServiceException(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_DELETE, exception);
-		}
-	}
+    /**
+     * Operations before update operation
+     *
+     * @param oldEntity old data on data source
+     * @param newEntity new data that will be saved to data source
+     * @return
+     */
+    protected abstract E preUpdate(E oldEntity, E newEntity);
 
-	@Override
-	public boolean deleteAll() {
+    @Override
+    public boolean delete(E entity) {
+        try {
 
-		// get repo
-		JpaRepository<E, ID> repository = getRepository();
+            // get repo
+            JpaRepository<E, ID> repository = getRepository();
 
-		try {
-			// delete entity
-			repository.deleteAll();
-			return true;
-		} catch (Exception exception) {
-			throw new ServiceException(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_DELETE, exception);
-		}
-	}
+            // delete entity
+            repository.delete(entity);
+            return true;
+        } catch (Exception exception) {
+            throw new ServiceException(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_DELETE, exception);
+        }
+    }
+
+    @Override
+    public boolean deleteAll() {
+
+        // get repo
+        JpaRepository<E, ID> repository = getRepository();
+
+        try {
+            // delete entity
+            repository.deleteAll();
+            return true;
+        } catch (Exception exception) {
+            throw new ServiceException(OctocloudMsCoreConstants.ERROR_MESSAGE_ENTITY_DELETE, exception);
+        }
+    }
 }
