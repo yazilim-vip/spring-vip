b982060ad462308d2aef186d582e1ae1a7ba9f2c
Emre Sen <emre.sen@cantekgroup.com>
2019-10-25 10:15:54 +0300

    refactor: unused imports removed.

--- src/main/java/cloud/cantek/ms/core/util/ObjectHelper.java
+++ src/main/java/cloud/cantek/ms/core/util/ObjectHelper.java
@@ -2,8 +2,6 @@ package cloud.cantek.ms.core.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationConfig;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class ObjectHelper {
 
