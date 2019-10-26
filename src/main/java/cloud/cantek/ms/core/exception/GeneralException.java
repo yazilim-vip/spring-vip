48c068f61d3b949682841eb5d2411efa5647efd7
Emre Sen <emre.sen@cantekgroup.com>
2019-10-26 16:49:49 +0300

    fix: exception ocnstructor updated.

--- src/main/java/cloud/cantek/ms/core/exception/GeneralException.java
+++ src/main/java/cloud/cantek/ms/core/exception/GeneralException.java
@@ -1,5 +1,6 @@
 package cloud.cantek.ms.core.exception;
 
+
 public class GeneralException extends Exception {
 
 	/**
@@ -7,12 +8,11 @@ public class GeneralException extends Exception {
 	 */
 	private static final long serialVersionUID = -7568063758377615867L;
 
-	public GeneralException(String message, Exception exception) {
-		super(message + " : " + ((exception == null) ? "" : exception.getMessage()));
+	public GeneralException(String message, Throwable throwable) {
+		super(message, throwable);
 	}
 
 	public GeneralException(String message) {
-		this(message, null);
+		super(message);
 	}
-
 }
