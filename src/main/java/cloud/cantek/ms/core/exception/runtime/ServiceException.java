54a84ec86fcee7686ddb21ba64bd822c2b0f74be
Emre Sen <emre.sen@cantekgroup.com>
2019-11-04 03:05:18 +0300

    style: unused TODO comments removed

--- src/main/java/cloud/cantek/ms/core/exception/runtime/ServiceException.java
+++ src/main/java/cloud/cantek/ms/core/exception/runtime/ServiceException.java
@@ -20,7 +20,6 @@ public class ServiceException extends MsRuntimeException {
 
 	public ServiceException(String message, Exception exception) {
 		super(message, exception);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ServiceException(Exception exception) {
