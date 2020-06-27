7516088dea15fe9d26a0919dc2b3c105e22678de
Emre Sen <maemresen@yazilim.vip>
2020-06-27 19:45:18 +0300

    refactor: redundant qualifier removed

--- src/main/kotlin/vip/yazilim/libs/springvip/config/SpringVipConfig.kt
+++ src/main/kotlin/vip/yazilim/libs/springvip/config/SpringVipConfig.kt
@@ -1,16 +1,10 @@
 package vip.yazilim.libs.springvip.config
 
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
-import org.springframework.web.servlet.HandlerMapping
-import vip.yazilim.libs.springvip.ext.logger
-import vip.yazilim.libs.springvip.rest.model.RestResponse
-import java.util.*
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-@Qualifier("springVipConfigurerAdapter")
 abstract class SpringVipConfig {
 
     abstract fun restResponseGenerator(responseBody: Any
