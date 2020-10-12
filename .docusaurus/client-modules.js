24a51f415a98c6617cba080d724b998333954154
Emre Sen <maemresen@yazilim.vip>
2020-10-12 15:15:29 +0300

    refactor

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/maemresen/workspace/yvip/spring-vip/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/maemresen/workspace/yvip/spring-vip/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/maemresen/workspace/yvip/spring-vip/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/maemresen/workspace/yvip/spring-vip/node_modules/infima/dist/css/default/default.css"),
-  require("/home/maemresen/workspace/yvip/spring-vip/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/home/maemresen/workspace/yvip/spring-vip/src/css/custom.css"),
-];
