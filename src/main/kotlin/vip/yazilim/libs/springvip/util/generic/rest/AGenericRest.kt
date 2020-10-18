8407901815e543ae34a5b6960f49b09ed293bb2a
Emre Sen <maemresen@yazilim.vip>
2020-10-18 18:30:13 +0300

    refactor

--- src/main/kotlin/vip/yazilim/libs/springvip/util/generic/rest/AGenericRest.kt
+++ src/main/kotlin/vip/yazilim/libs/springvip/util/generic/rest/AGenericRest.kt
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import kotlin.reflect.KClass
 
-/**.
+/**
  * Generic Implementation of a REST controller
  */
 abstract class AGenericRest<E : Any, ID : Any>(
